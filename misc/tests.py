850128271dfc360b5d89007ed0e572eb251d980b
jero98772 <jero98772@protonmail.com>
2021-10-01 09:35:46 -0500

    clear function in test.py

--- misc/tests.py
+++ misc/tests.py
@@ -4,17 +4,24 @@ class onFiles:
   def addHeader(self,folderDir):
     headerFolder=os.listdir(folderDir)
     for i in headerFolder:
-      if isFile(i):
-      else:
+      if isFile(i):pass
+      #else:
 extencions={".py":{"header":"#!/usr/bin/env python\n# -*- coding: utf-8 -*-\n","comentLine":"#"}}
-def recusrsiveSearch(folderDir,exts:list,aditionalInfo=[]:list):
+def recusrsiveSearch(folderDir,exts:list,proyectName="",ownerName=""):
   header=""
   headerFolder=os.listdir(folderDir)
   for i in headerFolder:
     if os.path.isFile(i):
-      extencion= len(i)-i[::-1].index(".")-1:
+      extencion= len(i)-i[::-1].index(".")-1
       header+=extencions[extencion]["header"]
-      for ii in aditionalInfo:
-        header+=extencions[extencion]["comentLine"]+ii+"\n"
+      if proyectName!="" and ownerName!="":
+        header+=extencions[extencion]["comentLine"]+proyectName+" - by "+ownerName+"\n"
+      elif proyectName=="" and ownerName!="":
+        header+=extencions[extencion]["comentLine"]+"by "+ownerName+"\n"
+      elif proyectName!="" and ownerName=="":
+        header+=extencions[extencion]["comentLine"]+"--"+proyectName+"--\n"
+      else:
+        pass
     else:
-      recusrsiveSearch(folderDir,exts,aditionalInfo)
+      recusrsiveSearch(folderDir,exts,proyectName,ownerName)
+#recusrsiveSearch(folderDir,exts,"cul","jero98772")#to test
