86154b62f984d91f4d9cd5eca648611bcfe839fa
jero98772 <jero98772@protonmail.com>
2022-08-29 20:56:22 -0500

    fix error ,add / on app.route

--- tools/tools.py
+++ tools/tools.py
@@ -125,7 +125,7 @@ class flaskWebProyect(generatorBase):
 from flask import Flask, render_template, request, flash, redirect ,session
 app = Flask(__name__)
 class webpage():
-  @app.route()
+  @app.route("/")
   def index():
     return render_template("index.html")
       """
